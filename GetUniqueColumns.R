125e9ecea83595d36af8e32efd70a27ba9422d03
Ben Linzmeier <bjlinzmeier@gmail.com>
2020-02-17 13:42:38 -0600

    Create GetUniqueColumns.R
    Get Unique column names for directory for column name replacement

---
+++ GetUniqueColumns.R
@@ -0,0 +1,43 @@
+GetUniqueColumns <- function(Directory=NA, Isotope){
+  
+  ####Function to get all unique column names in
+  ####a chosen directory from .xls? files.
+  ####Useful for writing batch import scripts
+  ####to easily combine data with non-uniform column names.
+
+  #Arguments:
+  #   Directory - File directory encompassing subdirectories of interest.
+  #   Isotope - grep for isotope system
+  # 
+  # Returns:
+  #   UniqueColumns - vector of unique column names sorted alphabetically
+  #   
+  #   
+  #   
+  #   
+
+  library(readxl)
+  
+  if(is.na(Directory)){
+    
+    Directory <- choose.dir()
+    
+  }
+  
+  FileList <- list.files(path = Directory, pattern = ".xls[x]?", recursive = TRUE, full.names = TRUE)
+  
+  FileList <- grep(Isotope, FileList, value = TRUE)
+  
+  InputColumns <- vector()
+  
+  for(i in 1:length(FileList)){
+    
+    InputColumns<-append(InputColumns, colnames(read_excel(FileList[i])))
+    
+  }
+  
+  UniqueColumns <- unique(InputColumns)
+  UniqueColumns <- sort(UniqueColumns)
+  
+  return(UniqueColumns)
+}
