ac5799fe0439623d1178bcf1eb341eed9c1acf24
thefallingduck <bjlinzmeier@gmail.com>
2021-06-09 16:58:46 -0500

    Added API security for importe

--- SparrowReformater.R
+++ SparrowReformater.R
@@ -1,11 +1,13 @@
 DatumNesting <- function(InputFile,
                          PlugNum = NA,
-                         Upload = TRUE) {
+                         Upload = TRUE,
+                         Token = "",
+                         ID = "") {
   #### Args:
   #           InputFile:
   #           Isotope method:
   #           Project:
-  
+
   #### Returns:
   #           UploadStatus:
   #This might be where we can change things to do different sample mounts?
@@ -18,44 +20,49 @@ DatumNesting <- function(InputFile,
   #PlugNum = NA
   #Upload = FALSE
   source("GeneralSIMSImporter.R")
-  
+
   BaseFile <- basename(InputFile)
   analysis <- list()
-  
+
   Output <-
     GeneralSIMSImporter(InputFile = InputFile, PlugNum = PlugNum)
   Output <- Output[!is.na(Output$File), ]
   Output$GUESS.SAMP <- as.factor(Output$GUESS.SAMP)
   Output$GUESS.SAMP <- droplevels(Output$GUESS.SAMP)
   Output <- Output[!is.na(Output$File)&!is.na(Output$DTFAX),]
-  
+
   print(colnames(Output))
-  
+
   IsotopeMethod <- ""
-  
+
   if (grepl("d18O", InputFile)) {
     IsotopeMethod <- "d18O10"
-    
+
   }
-  
+
   if (grepl("d13C", InputFile)) {
     IsotopeMethod <- "d13C7"
-    
+
   }
-  
+
   url1 <-
     "https://github.com/EarthCubeGeochron/Sparrow-WiscSIMS/blob/master/Test-Data/WiscSIMSColumnDictionary.xlsx?raw=true"
   GET(url1, write_disk(tf <- tempfile(fileext = ".xlsx")))
-  
+
   LookupDF <- read_excel(tf)
-  
-  
+
+if (Upload) {
+  headers = c('Content-Type'="application/json",
+              'Authorization'=Token)
+}
+
+
   #Make nested list for JSON format and Sparrow uploading
   m <- 0
   n <- 0
-  
+
   SampleList <- list()
-  
+
   for (k in 1:length(levels(Output$GUESS.SAMP))) {
     SampleList <- list()
     print(levels(Output$GUESS.SAMP)[k])
@@ -68,7 +75,7 @@ DatumNesting <- function(InputFile,
     analysis <- list()
     #DatumList <- list()
     #AttributeList <- list()
-    
+
     for (j in 1:nrow(SmallTable)) {
       #analysis <- list()
       DatumList <- list()
@@ -83,7 +90,7 @@ DatumNesting <- function(InputFile,
           } else{
             value <- -1042
           }
-          
+
           DatumList [[m]] <- list(
             value = value,
             type = list(
@@ -92,10 +99,10 @@ DatumNesting <- function(InputFile,
                                      colnames(SmallTable)[i]]
             )
           )
-          
+
           m <- m + 1
         }
-        
+
         if (LookupDF$Type[LookupDF$ColNames == colnames(SmallTable)[i]] ==
             "Text") {
           if (!is.na(SmallTable[j, i]) & !is.numeric(SmallTable[j, i])) {
@@ -103,23 +110,23 @@ DatumNesting <- function(InputFile,
           } else{
             note <- "Empty"
           }
-          
+
           AttributeList[[n]] <-
             list(parameter = colnames(SmallTable)[i],
                  value = note)
-          
+
           n <- n + 1
         }
-        
-        
+
+
         #print(m)
       }
-      
+
       d <- try(as.Date(SmallTable$DATETIME[i], format="%Y-%m-%d %H:%M:%S"))
       if("try-error" %in% class(d) || is.na(d)) {
 
         if (SmallTable$MATERIAL[j] == "STD") {
-          
+
           analysis[[j]] <- list(
             analysis_name = SmallTable$Comment[j],
             analysis_type = IsotopeMethod,
@@ -130,9 +137,9 @@ DatumNesting <- function(InputFile,
             session_index = j,
             standard_sample = list(name = SmallTable$RegexSTD[j])
           )
-          
+
         } else{
-          
+
           analysis[[j]] <- list(
             analysis_name = SmallTable$Comment[j],
             analysis_type = IsotopeMethod,
@@ -142,15 +149,15 @@ DatumNesting <- function(InputFile,
             material = SmallTable$MATERIAL[j],
             session_index = j
           )
-          
+
         }
-        
+
       } else {
-        
+
         date <- gsub(" ", "T", SmallTable$DATETIME[j])
-        
+
         if (SmallTable$MATERIAL[j] == "STD") {
-          
+
           analysis[[j]] <- list(
             analysis_name = SmallTable$Comment[j],
             analysis_type = IsotopeMethod,
@@ -162,9 +169,9 @@ DatumNesting <- function(InputFile,
             session_index = j,
             standard_sample = list(name = SmallTable$RegexSTD[j])
           )
-          
+
         } else{
-          
+
           analysis[[j]] <- list(
             analysis_name = SmallTable$Comment[j],
             analysis_type = IsotopeMethod,
@@ -175,16 +182,16 @@ DatumNesting <- function(InputFile,
             material = SmallTable$MATERIAL[j],
             session_index = j
           )
-          
+
         }
       }
-      
+
       #print(j)
       #print(nrow(SmallTable))
-      
-      
+
+
     }
-    
+
     Session <- list(
       name = paste(BaseFile, levels(Output$GUESS.SAMP)[k], sep = "_"),
       sample = list(name = levels(Output$GUESS.SAMP)[k]),
@@ -192,7 +199,7 @@ DatumNesting <- function(InputFile,
       date = gsub(" ", "T", min(Output$DATETIME, na.rm = TRUE)),
       analysis = analysis
     )
-    
+
     # SampleList <- append(SampleList,
     #                      list(list(
     #                        name = BaseFile,
@@ -200,20 +207,34 @@ DatumNesting <- function(InputFile,
     #                        date = min(Output$DATETIME, na.rm = TRUE),
     #                        analysis = analysis,
     #                                            date = Output$DATETIME[1])))
-    
-    request <- list(filename = paste(BaseFile, k),
-                    data = Session)
-    
+
+    request <- jsonlite::toJSON(list(filename = paste(BaseFile, k),
+                    data = Session),
+                    auto_unbox = TRUE)
+                # instrument_session <- list(data = list(name = BaseFile))
+                #
+                #   print(toJSON(instrument_session, auto_unbox = TRUE, pretty = TRUE))
+                #
+                #     response <-
+                #       PUT(url = "http://backend:5000/api/v2/import-data/instrument_session",
+                #           body = instrument_session,
+                #           encode = "json") #PUT(url="http://backend:5000/api/v1/import-data/session", body=request, encode = "json")
+                #
+                #     errors <- content(response)
+                #
+                #     print(errors)
+
     if (Upload) {
       response <-
-        PUT(url = "http://backend:5000/api/v1/import-data/session",
+        PUT(url = "http://backend:5000/api/v2/import-data/models/session",
             body = request,
+            httr::add_headers(headers),
             encode = "json") #PUT(url="http://backend:5000/api/v1/import-data/session", body=request, encode = "json")
-      
+
       errors <- content(response)
-      
+
       print(errors)
-      
+
     } else{
       print("Make JSON")
       NewFile <- gsub("\\..*", "", BaseFile)
@@ -230,16 +251,16 @@ DatumNesting <- function(InputFile,
         auto_unbox = TRUE,
         pretty = TRUE
       )
-      
+
     }
-    
-    
-    
-    
+
+
+
+
   }
-  
+
   #PUT(url="http://backend:5000/api/v1/import-data/session", body=SampleList, encode = "json")
-  
+
   return(print("DONE"))#print("Sparrow upload", BaseFile))
   #return(SampleList)
 }
